d0419a083cb120c2cd844544da3adbcb4ca6e851
Tychuk-Studio <studio@tychuk.com>
2024-05-15 18:00:22 +0300

    Project 15052024

---
+++ app-files/data.js
@@ -0,0 +1,168 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-001-80004000-01",
+      "name": "001-80004000-01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.35540816669296227,
+        "pitch": 0.18776660815555601,
+        "fov": 1.470127193399725
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21570055912071773,
+          "pitch": 0.32860682136868924,
+          "rotation": 8.63937979737193,
+          "target": "2-003-80004000-01"
+        },
+        {
+          "yaw": -0.14394096011111657,
+          "pitch": 0.5498850149450387,
+          "rotation": 0.7853981633974483,
+          "target": "1-002-80004000-01"
+        },
+        {
+          "yaw": 0.2744390094324949,
+          "pitch": 0.8350078295198369,
+          "rotation": 0,
+          "target": "0-001-80004000-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.09678210480652183,
+          "pitch": 0.48861992349570826,
+          "title": "002",
+          "text": "проба 2"
+        }
+      ]
+    },
+    {
+      "id": "1-002-80004000-01",
+      "name": "002-80004000-01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.729024275790363,
+        "pitch": 0.5832937377887557,
+        "fov": 1.470127193399725
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.655147892978885,
+          "pitch": 0.5160028423947551,
+          "rotation": 0,
+          "target": "0-001-80004000-01"
+        },
+        {
+          "yaw": 2.1052515193178483,
+          "pitch": 1.4902990446831756,
+          "rotation": 0,
+          "target": "1-002-80004000-01"
+        },
+        {
+          "yaw": -0.4428388886808552,
+          "pitch": 0.6404557602131788,
+          "rotation": 0,
+          "target": "2-003-80004000-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-003-80004000-01",
+      "name": "003-80004000-01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.6090788694960718,
+        "pitch": 0.269547227803864,
+        "fov": 1.470127193399725
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8945283011745353,
+          "pitch": 1.1958825830816995,
+          "rotation": 0,
+          "target": "2-003-80004000-01"
+        },
+        {
+          "yaw": 1.2799476803385712,
+          "pitch": 0.6487275325080706,
+          "rotation": 0,
+          "target": "1-002-80004000-01"
+        },
+        {
+          "yaw": 1.3023437906360318,
+          "pitch": 0.3225643655614654,
+          "rotation": 0,
+          "target": "0-001-80004000-01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
